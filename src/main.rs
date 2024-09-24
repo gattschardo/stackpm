9819c3aa3ad0fdb0cd83cc4bfe8f3633666de0c2
Richard Wind <gattschardo@gmail.com>
2024-09-24 16:59:22 +0200

    add parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
     println!("Hello, stackpm!");
+
+    let p = ast::parse("[imp] claim");
+    println!("{p:?}");
+    show(&mut p.unwrap());
+}
+
+fn show(es: &mut pest::iterators::Pairs<'_, ast::Rule>) {
+    for e in es {
+        for x in e.into_inner() {
+            println!("rule {:?}: {}", x.as_rule(), x.as_str());
+        }
+    }
+}
+
+mod ast {
+    use pest_derive::Parser;
+
+    #[derive(Parser)]
+    #[grammar = "spm.pest"]
+    struct SPMParser;
+
+    pub fn parse(i: &str) -> Result<pest::iterators::Pairs<'_, Rule>, pest::error::Error<Rule>> {
+        use pest::Parser;
+        SPMParser::parse(Rule::program, i)
+    }
 }
